c983c766441162147beddc3af4e5c6a000e4a860
rejchev <hendlerq@yandex.ru>
2022-12-07 22:23:52 +0500

    fix overflowing

--- src/lab1/lab1.py
+++ src/lab1/lab1.py
@@ -2,8 +2,9 @@ import numpy
 
 
 def foo():
-    x = range(9999999)
-    print(f"Сумма [0-9999999]: {numpy.sum(x)}; "
+    x = numpy.arange(9999999, dtype=object)
+
+    print(f"Сумма [0-9999999]: {x.sum()}; \n"
           f"Среднее: {numpy.average(x)}")
     return
 
@@ -12,7 +13,7 @@ def bar():
     x = numpy.random.randint(0, 100, size=10000)
 
     print(f"\n{x}")
-    print(f"Произведение 10000 случ. чисел в [0, 100): {numpy.prod(x)}; "
+    print(f"Произведение 10000 случ. чисел в [0, 100): {x.astype(object).prod()}; \n"
           f"Медиана 10000 случ. чисел: {numpy.median(x)}")
 
     return
