9bf6c33313a5e90953d498cb5237de3b77076b27
rejchev <hendlerq@yandex.ru>
2022-12-14 23:03:01 +0500

    add median & remove Y terms

--- src/lab3/lab3.py
+++ src/lab3/lab3.py
@@ -2,19 +2,27 @@ from pyDatalog import pyDatalog as PDL
 from numpy import random as RND
 
 
-PDL.create_terms('result, X, average, mult, median, summ, Y')
+PDL.create_terms('result, X, average, mult, median, summ')
 
 summ[X] = X + summ[X - 1]
 summ[1] = 1
 
+print(f"Sum [0 - 100]: {(summ[99] == X).v()[0]}")
+
 average[X] = X / 2
 
-mult[X] = RND.randint(0, 100) * mult[X - 1]
-mult[1] = 1
-
-print(f"Sum [0 - 100]: {(summ[99] == X).v()[0]}")
 print(f"Average [0 - 100]: {(average[99] == X).v()[0]}")
 
 print()
 
+mult[X] = RND.randint(0, 100) * mult[X - 1]
+mult[1] = 1
+
 print(f"Multiple (rand 100 elems in [0, 100)): {(mult[99] == X).v()[0]}")
+
+
+result[X] = RND.randint(1, 100) + result[X - 1]
+result[0] = 0
+
+median[X] = result[X] / X
+print(f"Median (rand 100 elems in [0,100): {(median[99] == X).v()[0]}")
